ccd3135057e950fa728577f6c32a0be7fd9b923c
Andy Cross <andy@elastacloud.com>
2020-03-31 21:43:38 +0100

    the honking is getting out of control ...

--- Services/NowPlayingService.cs
+++ Services/NowPlayingService.cs
@@ -17,33 +17,6 @@ namespace goose2s.Services {
             this.queue = queue;
         }
 
-        public async Task<QueueItem> Tick() {
-            if (nowPlaying == null || nowPlaying.PlayConcluded > 0L)
-            {
-                return await SetupNextTrack(queue);
-            }
-            else if (nowPlaying.PlayCommenced != 0L && (nowPlaying.PlayConcluded == 0L && (nowPlaying.PlayCommenced + (nowPlaying.DurationMs*10000L) < DateTime.UtcNow.Ticks)))
-            {
-                nowPlaying.PlayConcluded = DateTime.UtcNow.Ticks;
-                return await SetupNextTrack(queue);    
-            }
-            return nowPlaying;
-        }
-        private async Task<QueueItem> SetupNextTrack(QueueService queue)
-        {
-            var currentQueue = await queue.GetFirst("static", -1);
-            if (currentQueue.Item1 != long.MinValue)
-            {
-                sequenceNumber = currentQueue.Item1;
-                if (currentQueue.Item2 != null && nowPlaying != currentQueue.Item2) {
-                    nowPlaying = currentQueue.Item2;
-                    nowPlaying.PlayCommenced = DateTime.UtcNow.Ticks;
-                    if (TrackChanged != null) {
-                        TrackChanged(nowPlaying);
-                    }
-                }
-            }
-            return nowPlaying;
-        }
+        
     }
 }
--- Services/QueueService.cs
+++ Services/QueueService.cs
@@ -7,56 +7,6 @@ using System;
 
 namespace goose2s.Services {
     public class QueueService {
-        private QueueItem[] _queue = null;
-        private long _sequenceNumber = 0L;
-
-        public QueueService()
-        {
-            this._queue = new QueueItem[0];
-            this._sequenceNumber = DateTime.UtcNow.Ticks;
-        }
-
-        public async Task Enqueue(string flock, SpotifyItem track) {
-            var entity = new QueueItem(flock, DateTime.UtcNow) 
-            {
-                ArtistName = string.Join(", ", track.artists.Select(a=>a.name)),
-                DurationMs = track.duration_ms,
-                Image = track.album.images.OrderBy(i => i.height).First().url,
-                Popularity = track.popularity,
-                TrackId = track.id,
-                TrackName = track.name,
-                Votes = 0
-            };
-
-            lock (this._queue) {
-                var q =  new List<QueueItem>(_queue);
-                q.Add(entity);
-                this._queue = q.ToArray();
-                _sequenceNumber = DateTime.UtcNow.Ticks;
-            }
-        }
-
-        public async Task<(long, QueueItem[])> GetQueue(string flock, long sequenceNumber) {
-            if (_sequenceNumber == sequenceNumber)
-                return (long.MinValue, null);
-
-            return (_sequenceNumber, _queue
-                .Where(qi=> qi.PartitionKey == flock && string.Compare(qi.RowKey, DateTime.UtcNow.Ticks.ToString()) > 0
-                            && qi.PlayConcluded == 0L)
-                .ToArray());
-           
-        } 
         
-        public async Task<(long, QueueItem)> GetFirst(string flock, long sequenceNumber) {
-            if (_sequenceNumber == sequenceNumber)
-                return (long.MinValue, null);
-
-            return (_sequenceNumber, _queue
-                .Where(qi=> qi.PartitionKey == flock 
-                                && string.Compare(qi.RowKey, DateTime.UtcNow.Ticks.ToString()) > 0
-                                && (qi.PlayConcluded == 0L || qi.PlayConcluded > DateTime.UtcNow.Ticks))
-                .FirstOrDefault());
-           
-        } 
     }
 }
