d39cc55fa5c0cbdd0dfd27920beda7a5f11b295c
Andy Cross <andy@elastacloud.com>
2020-04-02 16:13:10 +0100

    nullable heights

--- Models/Image.cs
+++ Models/Image.cs
@@ -2,9 +2,9 @@ namespace goose2s.Models
 {
     public class Image
     {
-        public int height { get; set; }
+        public int? height { get; set; }
         public string url { get; set; }
-        public int width { get; set; }
+        public int? width { get; set; }
     }
 
 }
